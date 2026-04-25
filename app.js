c25de1c69526985f2f99c95c958839d2e794915c
NullAdmin123 <117506725+NullAdmin123@users.noreply.github.com>
2026-04-25 13:49:02 +0800

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const NEZHA_SERVER = process.env.NEZHA_SERVER || '';
 const NEZHA_PORT = process.env.NEZHA_PORT || '443';        // 端口为443时自动开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '';             // 哪吒三个变量不全不运行
 const DOMAIN = process.env.DOMAIN || '#DOMAIN#';  //项目域名或已反代的域名，不带前缀，建议填已反代的域名
-const NAME = process.env.NAME || 'wssub';
+const NAME = process.env.NAME || '免费-WM-';
 const port = process.env.PORT || 3000;
 
 // 创建HTTP路由
